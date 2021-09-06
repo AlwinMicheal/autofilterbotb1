b414873756289660565ccefb708c0c1e6c44cff6
AlwinMicheal <90100045+AlwinMicheal@users.noreply.github.com>
2021-09-06 11:16:10 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -4,47 +4,15 @@
 
 class Translation(object):
     
-    START_TEXT = """<b>Hey {}!!</b>
-<i>Am Just A Advance Auto Filter Bot....😉
+    START_TEXT = """<b>Hey {} My Name Is 🔘 NazriyaQueen 🔘 !!</b>
+<b>ഞാൻ @movies_hut_chat 👈 ഈ ഗ്രൂപ്പിന്റെ auto filter ആണ്.
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+എന്നെ വേറെ ഒരു ഗ്രൂപ്പിലും ഉപയോഗിക്കാൻ കഴിയില്ല ☺️. New മൂവീസ് വേണേൽ @new_movies_HD_movies ഈ ചാനലിലും.. ചോദിക്കുന്ന ഏത് ഭാഷയിലുമുള്ള മൂവീസ് വേണേൽ @movies_hut_chat ഈ ഗ്രൂപ്പിലും കേറിക്കോ.... അപ്പൊ എങ്ങനാ പൊളിക്കുവല്ലേ 😊🙃
 
-For More Details Click Help Button Below..
-@CrazyBotsz
-</i>"""    
+@movies_hut_chat
+</b>"""    
     
-    HELP_TEXT = """
-<b><i><u>How To Use Me!?</u></i></b>
-
-<i>
--> Add Me To Any Group And Make Me Admin
--> Add Me To Your Desired Channel
-</i>
-
-<b>Bot Commands (Works Only In Groups) :</b>
-
-    -> <code>/add chat_id</code>
-                OR                  - To Connect A Group With A Channel (Bot Should Be Admin With Full Previlages In Both Group And Channel)
-     <code>/add @Username</code>
-     
-    -> <code>/del chat_id</code>
-                OR                  - To disconnect A Group With A Channel
-     <code>/del @Username</code>
-     
-    -> <code>/delall</code>  - This Command Will Disconnect All Connected Channel With The Group And Deletes All Its File From DB
     
-    -> <code>/settings</code> -  This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly
-
-            -> <code>Channel</code> - Button Will Show You All The Connected Chats With The Group And Will Show Buttons Correspnding To There Order For Furthur Controls
-            
-            -> <code>Filter Types</code> - Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart
-
-            -> <code>Configure</code> - Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results
-            
-            -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
-            
-@CrazyBotsz
-"""
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
