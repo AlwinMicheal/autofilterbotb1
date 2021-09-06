c67d22fe18b8ea5ddf91e40584abbe4d7515f1f0
AlwinMicheal <90100045+AlwinMicheal@users.noreply.github.com>
2021-09-06 13:59:34 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,7 +28,11 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = f"{file_name} /n", 
+                caption = f"<b>{file_name}</b> /n <b>━═━═━═━═━═━═━═━═━═
+
+➠Cʜᴀɴɴᴇʟ : https://t.me/new_movies_HD_movies
+
+➠Gʀᴏᴜᴘ : https://t.me/movies_hut_chat</b>", 
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
